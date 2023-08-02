1c4a09784f530ec7f118a1fce1ba129139e2c73f
sadestyrahmadhani <sadestyrahmadhani012@gmail.com>
2023-08-02 10:39:14 +0700

    update login

--- src/component/navbar/index.js
+++ src/component/navbar/index.js
@@ -7,10 +7,22 @@ export default class extends Component {
             <>
                 <nav className="navbar navbar-xspand-lg bg-white shadow-sm py-3">
                     <div className="container">
-                        <img src="/images/iCareLogo.png" alt="logo" height="50"/>
+                        <img src="/images/iCareLogo.png" alt="logo" height="40"/>
                             <ul className="navbar-nav">
                                 <li className="nav-item">
-                                    <Link className="nav-link" to="dashboard"> Beranda </Link>
+                                    <Link className="nav-link" to=""> Beranda </Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className="nav-link" to=""> Tentang </Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className="nav-link" to=""> Fitur </Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className="nav-link" to=""> Keuntungan </Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className="nav-link" to=""> Testimoni </Link>
                                 </li>
                             </ul>
                     </div>
--- src/views/auth/login.js
+++ src/views/auth/login.js
@@ -1,29 +1,43 @@
 import { Component } from "react";
-import Navbar from "../../component/navbar";
-import FormInput from "./../../component/form/input"
+import { Link } from "react-router-dom";
+import FormInput from "./../../component/form/input";
 
 
 export default class extends Component {
     render() {
         return(
             <>
-                <div className="col-md-4 col-sm-7 col-xs-10 col-12 my-5 mx-auto">
-                    <div className="card shadow-sm rounded">
-                        <div className="card-body">
-                            <div className="col-12 text-center my-3">
-                                <img src="/images/iCareLogo.png" alt="Logo iCare" height="50" />
+                <div className="row my-5">
+                    <div className="col-9">
+                        <div className="col-sm-10 col-12 my-5 ">
+                            <div className="card shadow-sm rounded">
+                                <div className="card-body">
+                                    <div className="col-12 text-center my-3">
+                                        <img src="/images/iCareLogo.png" alt="Logo iCare" height="50" />
+                                    </div>
+                                    <form>
+                                        <div className="mb-3">
+                                            <label className="text-primary fs-6">EMAIL</label>
+                                            <input type="text" className="form-control border-only-bottom"/>
+                                        </div>
+                                        <div className="mb-4">
+                                            <label className="text-primary">PASSWORD</label>
+                                            <input type="password" className="form-control border-only-bottom" />
+                                        </div>
+                                        <div className="mb-3 mx-auto text-center">
+                                            <button className="btn btn-primary px-5 py-1">LOGIN</button>
+                                        </div>
+                                        <div className="text-center">
+                                            <Link className="nav-link" to="">Belum Punya akun ?</Link><br/>
+                                            <Link className="nav-link"  to="">Lupa Password ?</Link>
+                                        </div>
+                                    </form>
+                                </div>
                             </div>
-                            <form>
-                                <div className="mb-3">
-                                    <label className="text-primary">EMAIL</label>
-                                    <input type="text" className="form-control border-only-bottom"/>
-                                </div>
-                                <div className="mb-3">
-                                    <label className="text-primary">PASSWORD</label>
-                                    <input type="password" className="form-control border-only-bottom" />
-                                </div>
-                            </form>
                         </div>
+                    </div>
+                    <div className="background col-3">
+                        <img src="/images/Cahyo_MFD.png" alt="images 1" width="100%" />
                     </div>
                 </div>
             </>
