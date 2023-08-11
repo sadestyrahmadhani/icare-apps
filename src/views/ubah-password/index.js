fdbe65937c4b962dedfea71231ecfb0bb7d69921
mohammaddyvani <mdyvani@gmail.com>
2023-08-11 15:55:58 +0700

    fixs all

--- src/views/ubah-password/index.js
+++ src/views/ubah-password/index.js
@@ -1,5 +1,4 @@
 import { Component } from "react";
-// import {  } from "module";
 
 export default class extends Component {
 
@@ -22,7 +21,7 @@ export default class extends Component {
         return(
             <>
                 <div className="container">
-                    <div className="card-title text-center mx-auto my-4 mb-4" style={{borderBottom: '3px solid #014C90', width: '185px'}}>
+                    <div className="card-title text-center mx-auto my-4 mb-4" style={{borderBottom: '3px solid #014C90', width: '135px'}}>
                         <h4 className="title-icare fw-bold" style={{fontSize: '18px'}}>Ubah Kata Sandi</h4>
                     </div>
                     <div className="col-md-7 col-sm-5 col-12 mx-auto">
@@ -30,21 +29,21 @@ export default class extends Component {
                             <div className="card-body p-5">
                                 <form>
                                     <div className="mb-3">
-                                        <label className="fw-medium form-control border border-dark" style={{backgroundColor: '#014C90', borderRadius: '0px', color:'white', fontSize: '12px'}}>Masukkan Kata Sandi Lama</label>
+                                        <label className="fw-medium form-control border border-dark size-13px" style={{backgroundColor: '#014C90', borderRadius: '0px', color:'white'}}>Masukkan Kata Sandi Lama</label>
                                         <div className="d-flex border border-dark">
                                             <input type="password" className="form-control" style={{boxShadow: 'none', textDecoration: 'none'}}/>
                                             <i className="fa fa-eye fa-lg my-auto me-2"></i>
                                         </div>  
                                     </div>
                                     <div className="mb-3">
-                                        <label className="fw-medium form-control border border-dark" style={{backgroundColor: '#014C90', borderRadius: '0px', color:'white', fontSize: '12px'}}>Masukkan Kata Sandi Baru</label>
+                                        <label className="fw-medium form-control border border-dark size-13px" style={{backgroundColor: '#014C90', borderRadius: '0px', color:'white'}}>Masukkan Kata Sandi Baru</label>
                                         <div className="border border-dark d-flex">
                                             <input type="password" className="form-control" style={{boxShadow: 'none', textDecoration: 'none'}}/>
                                             <i className="fa fa-eye fa-lg my-auto me-2"></i>
                                         </div>
                                     </div>
                                     <div className="mb-4">
-                                        <label className="fw-medium form-control border border-dark" style={{backgroundColor: '#014C90', borderRadius: '0px', color:'white', fontSize: '12px'}}>Ulangi Kata Sandi Baru</label>
+                                        <label className="fw-medium form-control border border-dark size-13px" style={{backgroundColor: '#014C90', borderRadius: '0px', color:'white'}}>Ulangi Kata Sandi Baru</label>
                                         <div className="d-flex border border-dark">
                                             <input type="password" className="form-control" style={{boxShadow: 'none', textDecoration: 'none'}}/>
                                             <i className="fa fa-eye fa-lg my-auto me-2"></i>
@@ -52,7 +51,7 @@ export default class extends Component {
                                         </div>
                                     </div>
                                     <div className="text-center">
-                                        <button className="btn btn-login px-5 py-2fw-medium">Submit</button>
+                                        <button className="btn btn-login px-5 py-2 fw-medium">Submit</button>
                                     </div>
                                 </form>
                             </div>
