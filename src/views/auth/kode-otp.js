9763bb68d3ae004ffde8f412abb182f59a6b8495
mohammaddyvani <mdyvani@gmail.com>
2023-09-27 18:01:52 +0700

    update news, product

--- src/views/auth/kode-otp.js
+++ src/views/auth/kode-otp.js
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import { Component, useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Navbar from "../../component/navbar";
 import Footer from "../../component/footer";
@@ -7,65 +7,88 @@ import { verifyOtp } from "../../services";
 import { reSendOtp } from "../../services/API/mod_verifyOtp";
 import LoadingAlert from "../../component/alert/loadingAlert";
 
-export default class extends Component {
-    constructor(props) {
-        super(props)
-        this.submit = this.submit.bind(this)
-        this.handlePopup = this.handlePopup.bind(this)
-        this.handleReSendOtp = this.handleReSendOtp.bind(this)
-        this.resetIteration = this.resetIteration.bind(this)
-        this.state = {
-            iteration: 60,
-            resendCodeDisabled:true,
-            showPopup: false,
-            loading: false,
-            otp: '',
-            titleMessage:'',
-            errorMessage:'',
-            userInfo: JSON.parse(window.atob(this.props.router.params.id))
-        }
+function Otp() {
+    const navigate = useNavigate();
+    // const {userid, email, password, telp, msg, firstlogin }= this.props.params;
+    // this.submit = this.submit.bind(this)
+    // this.handlePopup = this.handlePopup.bind(this)
+    // this.handleReSendOtp = this.handleReSendOtp.bind(this)
+    // this.resetIteration = this.resetIteration.bind(this)
+    // console.log(this.props)
+    const location=useLocation()    
+    const [iteration, setIteration]= useState(60)       
+    const [resendCodeDisabled,setresendCodeDisabled]=useState(true)
+    const [showPopup,setshowPopup]=useState( false)
+    const [loading,setLoading]=useState( false)
+    const [otp,setotp]=useState( '')
+    const [titleMessage,settitleMessage]=useState('')
+    const [errorMessage,seterrorMessage]=useState('')
+    const [userid,setuserid]=useState(location.state.userid)
+    const [email,setemail]=useState(location.state.email)
+    const [password,setpassword]=useState(location.state.password)
+    const [telp,settelp]=useState(location.state.telp)
+    const [msg,setmsg]=useState(location.state.msg)
+    const [firstlogin,setfirstlogin]=useState(location.state.firstlogin)
+    
+        
+    useEffect(()=> {
+        
+        const interval=setInterval(() => {
+            setIteration(iteration - 1)
+            if(iteration === 0) {
+                setresendCodeDisabled(true)
+            } else {
+                setresendCodeDisabled(false)
+            }
+        }, 1000);
+        return () => clearInterval(interval);
+    },[iteration]);
+
+
+    // componentDidMount(){        
+    // }
+
+    const handlePopup= ()=>{
+        setshowPopup=false
     }
 
-    handlePopup(){
-        this.setState({showPopup:false})
-    }
-
-    async submit(e) {
+    async function useSubmit(e) {
         e.preventDefault()
-        this.setState({loading: true})
-        const res = await verifyOtp({userid: this.state.userInfo.userid, action: this.state.userInfo.action , otp: this.state.otp})
-        this.setState({loading: false})
+        const res = await verifyOtp({userid: userid, action: msg.action , otp: otp})
         if(res.status == 200 && res.data !== 'Succes, OTP match') {
-            this.setState({showPopup:true, errorMessage: res.data})
+            setshowPopup(true) 
+            seterrorMessage(res.data)
         } else {
-            if(this.state.userInfo.action === "Change Telp") {
+            if(msg.action === "Change Telp") {
                 localStorage.setItem('username', this.state.userInfo.name)
                 localStorage.setItem('telp', this.state.userInfo.telp)
-                this.props.router.navigate('/data_diri')
+                navigate('/data_diri')
             } else {
-                this.props.router.navigate('/update_password')
+                navigate('/ubah_kata_sandi')
             }
         } 
     } 
 
-    async handleReSendOtp() {
-        if(this.state.iteration === 0 ) {
-            const res = await reSendOtp({email: this.state.userInfo.email, telp: this.state.userInfo.telp, action: this.state.userInfo.action})
+    const handleReSendOtp = async () => {
+        if(iteration === 0 ) {
+            const res = await reSendOtp({email: email, telp: telp, action: msg.action})
             if(res.status == 200 && res.data === null) {
-                this.setState({showPopup: true, errorMessage:'Opps! terjadi kesalahan'})
+                setshowPopup(true)
+                seterrorMessage('Opps! terjadi kesalahan')
                 return
             }
             this.resetIteration()
         }
     }
 
-    resetIteration() {
-        if(this.state.iteration === 0) {
-            this.setState({iteration: 60, resendCodeDisabled: true})
+    const resetIteration= () => {
+        if(iteration === 0) {
+            setIteration(60)
+            setresendCodeDisabled(true)
         }
     }
+
     
-    render() {  
         return(
             <div className="bg-light">
                 <Navbar versi="2" />
@@ -83,11 +106,11 @@ export default class extends Component {
                                     style={{fontSize:'14px', textAlign:'justify'}}
                                 >
                                     Silahkan masukkan kode OTP yang telah dikirimkan melalui sms ke No. 
-                                    {this.state.userInfo.telp}
+                                    {telp}
                                 </p>
                                 <form 
                                     className="mb-3" 
-                                    onSubmit={this.submit}
+                                    onSubmit={useSubmit}
                                 >
                                     <input 
                                         className="form-control border-only-bottom custom-input-number text-center mb-4" 
@@ -105,14 +128,14 @@ export default class extends Component {
                                     </button>
                                 </form>
                                 <ConfirmAlert 
-                                    visible={this.state.showPopup} 
+                                    visible={showPopup} 
                                     titleMessage="Error"
-                                    message={this.state.errorMessage} 
-                                    onClick={this.handlePopup} 
+                                    message={errorMessage} 
+                                    onClick={handlePopup} 
                                     customClass="col-md-3 col-sm-6 col-7" 
                                 />
                                 <LoadingAlert 
-                                visible={this.state.loading}
+                                visible={loading}
                                 customClass="col-md-2 col-8"
                                 />
                                 <p 
@@ -122,9 +145,9 @@ export default class extends Component {
                                     Belum terima SMS kode OTP ?
                                 </p>
                                 <button 
-                                    className={`${this.state.resendCodeDisabled ? 'disabled' : 'fw-bold text-primary'}`} 
+                                    className={`${resendCodeDisabled ? 'disabled' : 'fw-bold text-primary'}`} 
                                     type="button" 
-                                    onClick={this.handleReSendOtp} 
+                                    onClick={handleReSendOtp} 
                                     style={{textDecoration:'none', color:'#9b9b9b', fontSize:'14px', border:'none', background:'transparent'}}
                                 >
                                     Kirim Ulang
@@ -133,25 +156,165 @@ export default class extends Component {
                                     className="mt-2" 
                                     style={{fontSize:'14px'}}
                                 >
-                                    Harap Tunggu {this.state.iteration} detik sebelum kirim ulang otp
+                                    Harap Tunggu {iteration} detik sebelum kirim ulang otp
                                 </p>
                             </div>
                         </div>
                     </div>
                 <Footer />
             </div>
-        )
-    }
+        ) 
 
-    componentDidMount() {
-        setInterval(() => {
-            this.setState({iteration:this.state.iteration === 0 ? 0 : this.state.iteration - 1})
-            if(this.state.iteration === 0) {
-                this.setState({resendCodeDisabled:false})
-            } else {
-                this.setState({resendCodeDisabled:true})
-            }
-        }, 1000)
-    }
 }
+export default Otp
+
+
+
+// export default class extends Component {
+//     constructor(props) {
+//         super(props)
+//         this.submit = this.submit.bind(this)
+//         this.handlePopup = this.handlePopup.bind(this)
+//         this.handleReSendOtp = this.handleReSendOtp.bind(this)
+//         this.resetIteration = this.resetIteration.bind(this)
+//         this.state = {
+//             iteration: 60,
+//             resendCodeDisabled:true,
+//             showPopup: false,
+//             loading: false,
+//             otp: '',
+//             titleMessage:'',
+//             errorMessage:'',
+//             userInfo: JSON.parse(window.atob(this.props.router.params.id))
+//         }
+//     }
+
+//     handlePopup(){
+//         this.setState({showPopup:false})
+//     }
+
+//     async submit(e) {
+//         e.preventDefault()
+//         this.setState({loading: true})
+//         const res = await verifyOtp({userid: this.state.userInfo.userid, action: this.state.userInfo.action , otp: this.state.otp})
+//         this.setState({loading: false})
+//         if(res.status == 200 && res.data !== 'Succes, OTP match') {
+//             this.setState({showPopup:true, errorMessage: res.data})
+//         } else {
+//             if(this.state.userInfo.action === "Change Telp") {
+//                 localStorage.setItem('username', this.state.userInfo.name)
+//                 localStorage.setItem('telp', this.state.userInfo.telp)
+//                 this.props.router.navigate('/data_diri')
+//             } else {
+//                 this.props.router.navigate('/update_password')
+//             }
+//         } 
+//     } 
+
+//     async handleReSendOtp() {
+//         if(this.state.iteration === 0 ) {
+//             const res = await reSendOtp({email: this.state.userInfo.email, telp: this.state.userInfo.telp, action: this.state.userInfo.action})
+//             if(res.status == 200 && res.data === null) {
+//                 this.setState({showPopup: true, errorMessage:'Opps! terjadi kesalahan'})
+//                 return
+//             }
+//             this.resetIteration()
+//         }
+//     }
+
+//     resetIteration() {
+//         if(this.state.iteration === 0) {
+//             this.setState({iteration: 60, resendCodeDisabled: true})
+//         }
+//     }
+    
+//     render() {  
+//         return(
+//             <div className="bg-light">
+//                 <Navbar versi="2" />
+//                     <div className="card-title text-center mx-auto my-3 mb-5 d-md-block d-none" style={{borderBottom:'3px solid #014C90', width:'92px'}}>
+//                         <h5 className="title-icare title-fitur fw-bold">Kode OTP</h5>
+//                     </div>
+//                     <div className="col-md-5 col-sm-8 col-12 mx-auto responsive-otp">
+//                         <div 
+//                             className="card mb-5 py-4 px-md-5 px-0 bg-light" 
+//                             style={{border:'1px solid'}}
+//                         >
+//                             <div className="card-body text-center px-4">
+//                                 <p 
+//                                     className="mb-2 text-otp" 
+//                                     style={{fontSize:'14px', textAlign:'justify'}}
+//                                 >
+//                                     Silahkan masukkan kode OTP yang telah dikirimkan melalui sms ke No. 
+//                                     {this.state.userInfo.telp}
+//                                 </p>
+//                                 <form 
+//                                     className="mb-3" 
+//                                     onSubmit={this.submit}
+//                                 >
+//                                     <input 
+//                                         className="form-control border-only-bottom custom-input-number text-center mb-4" 
+//                                         type="number" 
+//                                         onChange={(e) => this.setState({otp: e.target.value})} 
+//                                         placeholder="Masukkan Kode OTP" 
+//                                         style={{fontSize:'14px'}} 
+//                                     />
+//                                     <button 
+//                                     className="btn btn-login fw-medium rounded-3 shadow" 
+//                                     type="submit" 
+//                                     style={{fontSize:'14px', paddingLeft:'60px', paddingRight:'60px', paddingTop:'12px', paddingBottom:'12px'}}
+//                                     >
+//                                         SUBMIT
+//                                     </button>
+//                                 </form>
+//                                 <ConfirmAlert 
+//                                     visible={this.state.showPopup} 
+//                                     titleMessage="Error"
+//                                     message={this.state.errorMessage} 
+//                                     onClick={this.handlePopup} 
+//                                     customClass="col-md-3 col-sm-6 col-7" 
+//                                 />
+//                                 <LoadingAlert 
+//                                 visible={this.state.loading}
+//                                 customClass="col-md-2 col-8"
+//                                 />
+//                                 <p 
+//                                     className="mb-2" 
+//                                     style={{fontSize:'14px'}}
+//                                 >
+//                                     Belum terima SMS kode OTP ?
+//                                 </p>
+//                                 <button 
+//                                     className={`${this.state.resendCodeDisabled ? 'disabled' : 'fw-bold text-primary'}`} 
+//                                     type="button" 
+//                                     onClick={this.handleReSendOtp} 
+//                                     style={{textDecoration:'none', color:'#9b9b9b', fontSize:'14px', border:'none', background:'transparent'}}
+//                                 >
+//                                     Kirim Ulang
+//                                 </button>
+//                                 <p 
+//                                     className="mt-2" 
+//                                     style={{fontSize:'14px'}}
+//                                 >
+//                                     Harap Tunggu {this.state.iteration} detik sebelum kirim ulang otp
+//                                 </p>
+//                             </div>
+//                         </div>
+//                     </div>
+//                 <Footer />
+//             </div>
+//         )
+//     }
+
+//     componentDidMount() {
+//         setInterval(() => {
+//             this.setState({iteration:this.state.iteration === 0 ? 0 : this.state.iteration - 1})
+//             if(this.state.iteration === 0) {
+//                 this.setState({resendCodeDisabled:false})
+//             } else {
+//                 this.setState({resendCodeDisabled:true})
+//             }
+//         }, 1000)
+//     }
+// }
 
